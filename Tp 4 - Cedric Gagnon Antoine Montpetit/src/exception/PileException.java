ea86a189d5a78c096abfd16948ed3cb26971a08e
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2021-04-13 14:01:53 -0400

    Java Doc exeptions

--- Tp 4 - Cedric Gagnon Antoine Montpetit/src/exception/PileException.java
+++ Tp 4 - Cedric Gagnon Antoine Montpetit/src/exception/PileException.java
@@ -1,12 +1,25 @@
 package exception;
 
+/**
+ * 
+ * @author Cedric Gagnon
+ * @version printemps 2021
+ *
+ */
 public class PileException extends RuntimeException
 {
+	/**
+	 * Construit une exeption de base
+	 */
 	public PileException()
 	{
 		super();
 	}
 
+	/**
+	 * Construit un exeption avec un message
+	 * @param message - le message
+	 */
 	public PileException(String message)
 	{
 		super(message);
--- Tp 4 - Cedric Gagnon Antoine Montpetit/src/exception/PiocheException.java
+++ Tp 4 - Cedric Gagnon Antoine Montpetit/src/exception/PiocheException.java
@@ -1,12 +1,25 @@
 package exception;
 
+/**
+ * 
+ * @author Cedric Gagnon
+ * @version printemps 2021
+ *
+ */
 public class PiocheException extends RuntimeException
 {
+	/**
+	 * Construit une exeption de base
+	 */
 	public PiocheException()
 	{
 		super();
 	}
 
+	/**
+	 * Construit un exeption avec un message
+	 * @param message - le message
+	 */
 	public PiocheException(String message)
 	{
 		super(message);
