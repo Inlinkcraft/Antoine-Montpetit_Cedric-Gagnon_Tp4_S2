2c2b4af45e5e6429272ae22844bec6e0d8a3b8d9
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2021-04-13 13:50:26 -0400

    Pioche Java doc

--- Tp 4 - Cedric Gagnon Antoine Montpetit/src/cartes/Pioche.java
+++ Tp 4 - Cedric Gagnon Antoine Montpetit/src/cartes/Pioche.java
@@ -5,14 +5,29 @@ import java.io.Serializable;
 import exception.PiocheException;
 import structures.pile.Pile;
 
-
+/**
+ * 
+ * @author Cedric Gagnon
+ * @version printemps 2021
+ *
+ */
 public class Pioche implements Serializable
 {
 	
+	/**
+	 * Compare to serialVersionUID value
+	 */
 	private static final long serialVersionUID = -5454905195453222565L;
 	
+	/**
+	 * La pioche
+	 */
 	private Pile pioche;
 	
+	/**
+	 * Crée une pioche avec un paquet défénit
+	 * @param paquet - le paquet
+	 */
 	public Pioche(PaquetDeCartes paquet) {
 		pioche = new Pile();
 		while(paquet.size()>0) {
@@ -20,24 +35,41 @@ public class Pioche implements Serializable
 		}
 	}
 	
+	/**
+	 * Pige une carte de la pioche
+	 * @return - une carte
+	 */
 	public Carte piger() {
 		return(pioche.size()>0?(Carte)pioche.depiler():null);
 	}
 	
+	/**
+	 * retourne la carte sur le dessus de la pioche
+	 * @return - la carte du dessu de la pioche
+	 */
 	public String consulterDessus() {
 		if(pioche.isEmpty())
 			throw new PiocheException("Pioche vide");
 		return(((Carte)pioche.getPremier()).toStringCarte());
 	}
 	
+	/**
+	 * Retourne vrai si la pioche est vide
+	 * @return - true si la pioche est vide
+	 */
 	public boolean isEmpty() {
 		return(pioche.isEmpty());
 	}
 	
+	/**
+	 * Retourne la grosseur de la pioche
+	 * @return - la grosseur de la pioche
+	 */
 	public int size() {
 		return(pioche.size());
 	}
 	
+	@Override
 	public String toString() {
 		return(pioche.getPremier().toString());
 	}
